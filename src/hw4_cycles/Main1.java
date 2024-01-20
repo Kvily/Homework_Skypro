45956de370428f185433ef90d6c6fa45b2db8219
Diana Safina <diana.safina.99@bk.ru>
2024-01-20 21:13:30 +0500

    Выполнена домашняя работа №6

---
+++ src/hw4_cycles/Main1.java
@@ -0,0 +1,13 @@
+package hw4_cycles;
+// ЦИКЛЫ
+public class Main1 {
+    public static void main(String[] args) {
+        //Задача 1
+        // *С помощью цикла for выведите в консоль все числа от 1 до 10.*//
+        for(int i = 1; i <= 10; i = i + 1){
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+}
+
+
---
+++ src/hw4_cycles/Main2.java
@@ -0,0 +1,12 @@
+package hw4_cycles;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        //Задача 2
+        //*С помощью цикла for выведите в консоль все числа от 10 до 1.*//
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+}
