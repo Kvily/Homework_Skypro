651ec3e27c6563bbedf33e51ae24dba099d36afa
Diana Safina <diana.safina.99@bk.ru>
2024-04-12 23:57:17 +0500

    Initial commit

--- src/main/java/pro/skycalculatorcalculator/HomeworkCalculatorApplication.java
+++ src/main/java/pro/skycalculatorcalculator/HomeworkCalculatorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeworkCalculatorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkCalculatorApplication.class, args);
 	}
